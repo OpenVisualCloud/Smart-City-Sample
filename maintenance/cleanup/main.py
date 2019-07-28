8f63165aff8c2945bd2af30808853e9245d92da3
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-07-28 18:25:29 -0500

    fix cleanup start code

--- maintenance/cleanup/main.py
+++ maintenance/cleanup/main.py
@@ -11,35 +11,28 @@ indexes=os.environ["INDEXES"].split(",")
 office=list(map(float, os.environ["OFFICE"].split(",")))
 dbhost=os.environ["DBHOST"]
 
-def start_service():
-    print("Starting the cleanup service...",flush=True)
-    while True: 
-        time.sleep(service_interval)
-
-        print("Searching...",flush=True)
-        for index in indexes:
-            db=DBQuery(index=index,office=office,host=dbhost)
-            try:
-                for r in db.search("time<now-"+str(retention_time*1000), size=500):
-                    # delete the record
-                    db.delete(r["_id"])
-    
-                    # delete the path file
-                    if "path" in r["_source"]:
-                        try:
-                            os.remove(r["_source"]["path"])
-                            os.remove(r["_source"]["path"]+".png")
-                        except Exception as e:
-                            pass
-            except Exception as e:
-                print("Exception: "+str(e), flush=True)
-
 def quit_service(signum, sigframe):
     exit(143)
 
-# set signal to quit nicely
 signal(SIGTERM, quit_service)
+while True: 
+    time.sleep(service_interval)
 
-while True:
-    start_service()
-    time.sleep(10)
+    print("Searching...",flush=True)
+    for index in indexes:
+        db=DBQuery(index=index,office=office,host=dbhost)
+        try:
+            for r in db.search("time<now-"+str(retention_time*1000), size=500):
+                # delete the record
+                db.delete(r["_id"])
+    
+                # delete the path file
+                if "path" in r["_source"]:
+                    try:
+                        os.remove(r["_source"]["path"])
+                        os.remove(r["_source"]["path"]+".png")
+                    except Exception as e:
+                        pass
+        except Exception as e:
+            print("Exception: "+str(e), flush=True)
+            time.sleep(10)
