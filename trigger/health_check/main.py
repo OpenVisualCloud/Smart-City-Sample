b42c73025b4c5d02fdf5ce2ec2f0b4392b159dad
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-07-28 18:13:13 -0500

    fix health check start code

--- trigger/health_check/main.py
+++ trigger/health_check/main.py
@@ -14,73 +14,6 @@ dbhost=os.environ["DBHOST"]
 dbt=None
 rt=None
 
-def start_service():
-    print("Start the health_check service...", flush=True)
-
-    # register trigger
-    global dbt
-    dbt=DBIngest(index="triggers",office=office,host=dbhost)
-    try:
-        global rt
-        rt=dbt.ingest({
-            "name": "health_check",
-            "status": "idle",
-        })
-
-        dbs=DBQuery(index="sensors",office=office,host=dbhost)
-        dba=DBQuery(index="algorithms",office=office,host=dbhost)
-        dbat=DBIngest(index="alerts",office=office,host=dbhost)
-    
-        try:
-            dbt.update(rt["_id"], { "status": "processing" })
-
-            while True:
-                try:
-                    # count #sensors
-                    nsensors={
-                        "total": dbs.count("sensor:*"),
-                        "streaming": dbs.count("status:'streaming'"),
-                        "idle": dbs.count("status:'idle'"),
-                    }
-                    print("nsensors",flush=True)
-                    print(nsensors,flush=True)
-
-                    nalgorithms={
-                        "total": dba.count("name:*"),
-                    }
-                    print("nalgorithms",flush=True)
-                    print(nalgorithms,flush=True)
-
-                    alerts={}
-                    if nsensors["total"]>nsensors["streaming"]+nsensors["idle"]:
-                        alerts["maintenance_required"]=nsensors
-
-                    if nalgorithms["total"]!=nsensors["streaming"]+nsensors["idle"]:
-                        alerts["balancing_required"]={
-                            "nalgorithms": nalgorithms["total"],
-                            "nsensors": nsensors["streaming"]+nsensors["idle"],
-                        }
-
-                    # ingest alerts
-                    if alerts:
-                        alerts["time"]=int(time.mktime(datetime.datetime.now().timetuple())*1000)
-                        alerts["trigger"]=rt["_id"]
-                        print(alerts,flush=True)
-                        dbat.ingest(alerts)
-
-                    time.sleep(service_interval)
-
-                except Exception as e:
-                    print("Exception "+str(e),flush=True)
-                    time.sleep(30)
-
-        except Exception as e:
-            print("Exception: "+str(e),flush=True)
-            dbt.delete(rt["_id"])
-
-    except Exception as e:
-        print("Exception: "+str(e),flush=True)
-   
 def quit_service(signum, sigframe):
     try:
         if dbt and rt: dbt.delete(rt["_id"])
@@ -90,6 +23,51 @@ def quit_service(signum, sigframe):
 
 signal(SIGTERM, quit_service)
 
+# register trigger
+dbt=DBIngest(index="triggers",office=office,host=dbhost)
 while True:
-    start_service()
-    time.sleep(10)
+    try:
+        rt=dbt.ingest({
+            "name": "health_check",
+            "status": "processing",
+        })
+        break
+    except Exception as e:
+        print("Exception: "+str(e), flush=True)
+        time.sleep(10)
+
+dbs=DBQuery(index="sensors",office=office,host=dbhost)
+dba=DBQuery(index="algorithms",office=office,host=dbhost)
+dbat=DBIngest(index="alerts",office=office,host=dbhost)
+    
+while True:
+    try:
+        nsensors={
+            "total": dbs.count("sensor:*"),
+            "streaming": dbs.count("status:'streaming'"),
+            "idle": dbs.count("status:'idle'"),
+        }
+        nalgorithms={
+            "total": dba.count("name:*"),
+        }
+   
+        alerts={}
+        if nsensors["total"]>nsensors["streaming"]+nsensors["idle"]:
+            alerts["maintenance_required"]=nsensors
+
+        if nalgorithms["total"]!=nsensors["streaming"]+nsensors["idle"]:
+            alerts["balancing_required"]={
+                "nalgorithms": nalgorithms["total"],
+                "nsensors": nsensors["streaming"]+nsensors["idle"],
+            }
+
+        # ingest alerts
+        if alerts:
+            alerts["time"]=int(time.mktime(datetime.datetime.now().timetuple())*1000)
+            alerts["trigger"]=rt["_id"]
+            dbat.ingest(alerts)
+
+   except Exception as e:
+       print("Exception "+str(e),flush=True)
+
+   time.sleep(service_interval)
