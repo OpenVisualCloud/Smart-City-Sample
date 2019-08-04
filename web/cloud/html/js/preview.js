c0ac573baf0ae8f91ba4cd30f5792b6005dee20a
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-08-04 08:45:48 -0700

    reformat video box

--- web/cloud/html/js/preview.js
+++ web/cloud/html/js/preview.js
@@ -13,6 +13,10 @@ var previews={
             }
             ctx.marker.closeTooltip();
             previews.play(div,sensor);
+
+            div.attr('draggable','true').bind('dragstart',function (e) {
+                e.originalEvent.dataTransfer.setData("application/json",JSON.stringify(sensor));
+            });
         });
     },
     play: function (div, sensor) {
@@ -24,13 +28,11 @@ var previews={
                     div.empty().append(error);
                     return setTimeout(update,5000);
                 }
-                var video=$('<video draggable="true" style="width:100%;height:100%" autoplay muted><source src="recording/'+r[0]._source.path+'?'+$.param({office:sensor._source.office.lat+","+sensor._source.office.lon})+'"></source></video>');
-                div.empty().append(video);
-                video.bind('ended',update).bind('error',function () {
+                var title=$('<div style="position:absolute;width:100%;top:0;text-align:center;font-size:1em">['+sensor._source.location.lat+","+sensor._source.location.lon+"]</div>");
+                var video=$('<video style="position:absolute;top:0;width:100%;height:100%" autoplay muted><source src="recording/'+r[0]._source.path+'?'+$.param({office:sensor._source.office.lat+","+sensor._source.office.lon})+'"></source></video>').bind('ended',update).bind('error',function () {
                     setTimeout(update,5000);
-                }).bind('dragstart',function (e) {
-                    e.originalEvent.dataTransfer.setData("application/json",JSON.stringify(sensor));
                 });
+                div.empty().append(title).append(video);
             }).catch(function () {
                 div.empty().append(error);
                 setTimeout(update,5000);
@@ -46,7 +48,7 @@ var previews={
             var sensor=JSON.parse(e.originalEvent.dataTransfer.getData("application/json"));
             var div=canvas.parent().find("[preview-template]").clone();
             div.removeAttr("preview-template").show();
-            var icon=L.divIcon({html:div[0]});
+            var icon=L.divIcon({html:div[0],iconSize:[300,200],shadowSize:[300,200]});
             var marker=L.marker(map.mouseEventToLatLng(e),{icon:icon,draggable:true}).addTo(layer);
             previews.play(div,sensor);
         });
