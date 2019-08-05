9d73e0ebc4c7ec8f958e4b121bdc6ecdafd66559
Pengfei Qu <pengei.qu@intel.com>
2019-08-05 05:43:07 -0400

    use the local Dockerfile

--- analytics/feeder/build.sh
+++ analytics/feeder/build.sh
@@ -1,19 +1,36 @@
 #!/bin/bash -e
 
 # Build VA Service from Github 
-AD_INSERTION_REPO="https://github.com/OpenVisualCloud/Ad-Insertion-Sample.git"
-VA_FOLDER="ad-insertion/video-analytics-service"
 USER="docker"
 GROUP="docker"
-
-sudo docker build ${AD_INSERTION_REPO}\#v1.1:${VA_FOLDER} --file platforms/Xeon/Dockerfile.2.gst.xeon --target build -t "xeon-ubuntu1804-dldt-gst-va:build" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
-
-sudo docker build ${AD_INSERTION_REPO}\#v1.1:${VA_FOLDER} --file platforms/Xeon/Dockerfile.2.gst.xeon -t "xeon-ubuntu1804-dldt-gst-va:latest" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
-
-sudo docker build ${AD_INSERTION_REPO}\#v1.1:${VA_FOLDER} --file platforms/Xeon/Dockerfile.1.va.gst.xeon -t "video_analytics_service_gstreamer:latest" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
 
 #IMAGE="smtc_analytics_recording"
 DIR=$(dirname $(readlink -f "$0"))
 
 cp -f "$DIR/../../script/db_query.py" "$DIR/../../script/db_ingest.py" "$DIR/../../script"/dsl_*.py "$DIR/../../script/probe.py" "$DIR"
 . "$DIR/../../script/build.sh"
+
+case "$1" in 
+    vcac-a)
+        PLATFORM=VCAC-A
+        ;;
+    *)
+        PLATFORM=Xeon
+        ;;
+esac
+
+# build image(s) in order (to satisfy dependencies)
+for dep in '.12.*' '.11.*' '.10.*' ''; do
+    for dockerfile in `find "${DIR}/platforms/${PLATFORM}" -maxdepth 1 -name "Dockerfile${dep}" -print`; do
+        image=$(head -n 1 "$dockerfile" | grep '# ' | cut -d' ' -f2)
+        if test -z "$image"; then image="$IMAGE"; fi
+
+        if grep -q 'AS build' "$dockerfile"; then
+            echo sudo docker build --file="$dockerfile" --target build -t "$image:build" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+            sudo docker build --file="$dockerfile" --target build -t "$image:build" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+        fi
+
+        echo sudo docker build --file="$dockerfile" -t "$image:latest" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+        sudo docker build --file="$dockerfile" -t "$image:latest" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+    done
+done
--- analytics/feeder/update.sh
+++ analytics/feeder/update.sh
@@ -1,7 +1,28 @@
 #!/bin/bash -e
 
+DIR=$(dirname $(readlink -f "$0"))
+XEON_DIR=$DIR/platforms/Xeon
+VCAC_A_DIR=$DIR/platforms/VCAC-A
+AD_INSERT_TAG=v1.1
+DOCKERFILE_TAG=master
+
+# ffmpeg base image on centos
 DOCKER_REPO=${DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.0/Xeon/centos-7.6"}
 
-DIR=$(dirname $(readlink -f "$0"))
+base_name=xeon-centos76-ffmpeg
+docker_file="$XEON_DIR/Dockerfile.1.ffmpeg"
 
-(echo "# xeon-centos76-ffmpeg"; curl ${DOCKER_REPO}/ffmpeg/Dockerfile) > "$DIR/Dockerfile.1.ffmpeg"
+echo "# xeon-centos76-ffmpeg" > ${docker_file}
+curl ${DOCKER_REPO}/ffmpeg/Dockerfile >> ${docker_file} 
+
+###################################################################################################################
+
+# gst va service base dockerfile
+DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Ad-Insertion-Sample/${AD_INSERT_TAG}/ad-insertion/video-analytics-service/platforms/Xeon/Dockerfile.1.va.gst.xeon"
+docker_file="$XEON_DIR/Dockerfile.11.gst.va.xeon"
+curl ${DOCKER_REPO} > ${docker_file}
+
+# gst base dockerfile
+DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Ad-Insertion-Sample/${AD_INSERT_TAG}/ad-insertion/video-analytics-service/platforms/Xeon/Dockerfile.2.gst.xeon"
+docker_file="$XEON_DIR/Dockerfile.12.gst.xeon"
+curl ${DOCKER_REPO} > ${docker_file}
