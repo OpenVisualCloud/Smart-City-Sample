f2df42be31a256419a802a891a580f9d743632a2
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-08-06 12:09:58 -0700

    make round corners

--- web/cloud/html/js/preview.js
+++ web/cloud/html/js/preview.js
@@ -25,6 +25,7 @@ var previews={
                     var icon=L.divIcon({html:div[0],iconSize:[300,200],iconAnchor:[0,0]});
                     var marker=L.marker(map.mouseEventToLatLng(e),{icon:icon,draggable:true}).addTo(layer);
                     marker._zoomargs={zoom:map.getZoom(),width:300,height:200};
+                    $(marker._icon).css({'border-radius':'10px'});
                     var sensor1=JSON.parse(e.originalEvent.dataTransfer.getData("application/json"));
                     previews.play(div,sensor1);
 
