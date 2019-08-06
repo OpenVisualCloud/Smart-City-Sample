dbfae95b174055cd55864aa5486f717f90729208
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-08-06 15:16:11 -0700

    probe stream for resolution in camera simulation

--- sensor/simulation/download.sh
+++ sensor/simulation/download.sh
@@ -3,35 +3,20 @@
 DIR=$(dirname $(readlink -f "$0"))
 CLIPS=(https://www.pexels.com/video/1388383/download,1388383.mp4,https://www.pexels.com/photo-license)
 
-declare -A ACCEPTS
-ACCEPTS["na"]="accept"
-
-printf "\n\n\n"
 for clip in "${CLIPS[@]}"; do
     url=$(echo "$clip" | cut -f1 -d',')
     clip_name=$(echo "$clip" | cut -f2 -d',')
     clip_mp4="${clip_name/\.*/}.mp4"
     license=$(echo "$clip" | cut -f3 -d',')
-    duration=$(echo "$clip" | cut -f4 -d',')
-    if test "$duration" -gt "0"; then
-        duration=(-t $duration)
-    else
-        duration=()
-    fi
 
     if test ! -f "$DIR/$clip_mp4"; then
-        if test "${ACCEPTS[$license]}" != "accept"; then
-            printf "The Smart City sample requires you to have a dataset to simulate camera, please accept downloading $url for camera simulation where data set subject to $license. The terms and conditions of the data set license apply. Intel does not grant any rights to the data files.\n\n\nPlease type \"accept\" or anything else to skip the download.\n"
-            read reply
-            ACCEPTS[$license]="$reply"
-        else
-            printf "Downloading $url for camera simulation...\n"
-        fi
-        if test "${ACCEPTS[$license]}" = "accept"; then
+        printf "\n\n\nThe Smart City sample requires you to have a dataset to simulate camera, please accept downloading dataset for camera simulation:\n\nDataset: $url\nLicense: $license\n\nThe terms and conditions of the data set license apply. Intel does not grant any rights to the data files.\n\n\nPlease type \"accept\" or anything else to skip the download.\n"
+        read reply
+        if test "$reply" = "accept"; then
             echo "Downloading..."
             tmp="tmp_$clip_name"
             wget -q -O "$DIR/$tmp" "$url"
-            sudo docker run --rm -u $(id -u):$(id -g) -v "$DIR:/mnt:rw" -it openvisualcloud/xeon-centos76-media-ffmpeg ffmpeg -i /mnt/$tmp -c:v libx264 -profile:v baseline -x264-params keyint=30:bframes=0 -c:a aac -ss 0 ${duration[@]} /mnt/$clip_mp4
+            sudo docker run --rm -u $(id -u):$(id -g) -v "$DIR:/mnt:rw" -it openvisualcloud/xeon-centos76-media-ffmpeg ffmpeg -i /mnt/$tmp -c:v libx264 -profile:v baseline -x264-params keyint=30:bframes=0 -c:a aac -ss 0 /mnt/$clip_mp4
             rm -f "$DIR/$tmp"
         else
             echo "Skipping..."
