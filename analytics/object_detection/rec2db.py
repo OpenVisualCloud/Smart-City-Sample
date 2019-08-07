8fc7808b5c86f0126c56036d1dd04bdaf6166ac2
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-08-07 15:46:31 -0700

    bug fixes

--- analytics/object_detection/rec2db.py
+++ analytics/object_detection/rec2db.py
@@ -7,6 +7,7 @@ from threading import Lock
 from probe import probe, run
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import psutil
 import os
 import tempfile
 import shutil
@@ -23,25 +24,23 @@ class Handler(FileSystemEventHandler):
         self._sensor = sensor
         self._db_rec = DBIngest(host=dbhost, index="recordings", office=office)
         self._db_cam = DBQuery(host=dbhost, index="sensors", office=office)
-        self._files = []
         self._lock = Lock()
-        self._timer = IntervalTimer(10, self.on_timer)
+        self._files = []
         self._last_file = None
     
-    def stop(self):
-        self._timer.cancel()
-
     def on_created(self, event):
+        print("on_created: "+event.src_path, flush=True)
         if event.is_directory: return
         if event.src_path.endswith(".png"): return
-        if self._last_file and (self._last_file==event.src_path): return
         if self._last_file:
+            if self._last_file==event.src_path: return
             self._lock.acquire()
             self._files.append(self._last_file)
             self._lock.release()
         self._last_file = event.src_path
 
     def ffmpeg_convert(self,filename): 
+        print("post-processing "+filename, flush=True)
         with tempfile.TemporaryDirectory() as tmpdirname:
             filename = os.path.abspath(filename)
             tmpfilename = os.path.abspath(os.path.join(tmpdirname,os.path.basename(filename)))
@@ -59,51 +58,59 @@ class Handler(FileSystemEventHandler):
         return int(int(parsed[-2])/1000000)
 
     def on_timer(self):
+        print("rec2db on_timer", flush=True)
         self._lock.acquire()
-        file1=self._files.pop() if self._files else None
+        files=self._files
+        self._files=[]
         self._lock.release()
 
-        if not file1: return
-        converted_file,sinfo=self.ffmpeg_convert(file1)
-        sinfo.update({
-            "sensor": self._sensor,
-            "office": {
-                "lat": office[0],
-                "lon": office[1],
-            },
-            "time": self.get_timestamp(converted_file),
-            "path": os.path.abspath(converted_file).split(os.path.abspath(storage)+"/")[1],
-        })
-        
-        # calculate total bandwidth
-        bandwidth=0
-        for stream1 in sinfo["streams"]:
-            if "bit_rate" in stream1: 
-                bandwidth=bandwidth+stream1["bit_rate"]
+        while files:
+            # yield CPU if the system is too busy
+            if psutil.cpu_percent()>60: time.sleep(5)
 
-        if bandwidth: self._db_cam.update(self._sensor, {"bandwidth": bandwidth})
-        self._db_rec.ingest(sinfo)
+            converted_file,sinfo=self.ffmpeg_convert(files.pop())
+            sinfo.update({
+                "sensor": self._sensor,
+                "office": {
+                    "lat": office[0],
+                    "lon": office[1],
+                },
+                "time": self.get_timestamp(converted_file),
+                "path": os.path.abspath(converted_file).split(os.path.abspath(storage)+"/")[1],
+            })
+        
+            # calculate total bandwidth
+            bandwidth=0
+            for stream1 in sinfo["streams"]:
+                if "bit_rate" in stream1: 
+                    bandwidth=bandwidth+stream1["bit_rate"]
+
+            if bandwidth: self._db_cam.update(self._sensor, {"bandwidth": bandwidth})
+            self._db_rec.ingest(sinfo)
+            print(sinfo, flush=True)
 
 class Rec2DB(object):
     def __init__(self, sensor):
         super(Rec2DB,self).__init__()
         self._sensor=sensor
         self._handler=Handler(sensor)
-        self._observer=None
+        self._timer = IntervalTimer(15, self._handler.on_timer)
+        self._observer=Observer()
         self._watcher=None
 
     def loop(self):
-        self._observer=Observer()
         self._observer.start()
+        self._timer.start()
 
-        folder=os.path.join(os.path.realpath(storage),sensor)
+        folder=os.path.join(os.path.realpath(storage),self._sensor)
+        print("Observing "+folder, flush=True)
         os.makedirs(folder, exist_ok=True)
         self._watcher=self._observer.schedule(self._handler, folder, recursive=True)
 
-        observer.join()
-        self._handler.stop()
+        self._observer.join()
+        self._timer.cancel()
 
     def stop(self):
-        if self._observer: 
-            if self._watcher: self._observer.unschedule(self._watcher)
-            self._observer.stop()
+        if self._watcher: self._observer.unschedule(self._watcher)
+        self._observer.stop()
+        self._timer.cancel()
