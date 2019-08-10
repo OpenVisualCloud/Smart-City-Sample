7a2b6dc15a834da41c615523ddcaa17d3fcb87c2
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-08-10 14:43:18 -0700

    restore to remote host

--- script/restore.sh
+++ script/restore.sh
@@ -2,6 +2,18 @@
 
 DIR=$(dirname $(readlink -f "$0"))
 
-for tarfile in "$DIR/../archive"/*.tar; do
-    sudo docker load -i "$tarfile"
-done
+if test -z "$1"; then
+    for tarfile in "$DIR/../archive"/*.tar; do
+        sudo docker load -i "$tarfile"
+    done
+else
+    read -p "Password: " -s pw
+    echo
+    for host1 in "$@"; do
+        echo "Transfer images to $host1"
+        for tarfile in "$DIR/../archive"/*.tar; do
+            # assume the host can ssh to host1 without password
+            echo $pw | cat - $tarfile | ssh $host1 cat \| sudo --prompt="" -S -- "docker load"
+        done
+    done
+fi
