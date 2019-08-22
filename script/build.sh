9ba23da55d76e8d2b6259950ea6dd2eadc4bfb78
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-08-22 09:42:43 -0700

    change current dir while building.

--- script/build.sh
+++ script/build.sh
@@ -17,7 +17,7 @@ build_docker() {
         m4 -I "$(dirname $docker_file)" "$docker_file.m4" > "$docker_file"
     fi
     echo sudo docker build --file="$docker_file" "$@" -t "$image_name" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
-    sudo docker build --file="$docker_file" "$@" -t "$image_name" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+    (cd "$DIR"; sudo docker build --file="$docker_file" "$@" -t "$image_name" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g))
 }
 
 # build image(s) in order (to satisfy dependencies)
