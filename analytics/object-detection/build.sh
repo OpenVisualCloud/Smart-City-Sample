7c4c7bad07efbb95ce2bb58626a0fd6111963747
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-09-04 23:46:52 -0700

    typo

--- analytics/object-detection/build.sh
+++ analytics/object-detection/build.sh
@@ -2,10 +2,10 @@
 
 DIR=$(dirname $(readlink -f "$0"))
 PLATFORM="${1:-Xeon}"
-SCENARIO="${2:-street}"
+SCENARIO="${2:-traffic}"
 
 case "$SCENARIO" in
-    street)
+    *traffic*)
         cp -f "$DIR/../../script/db_query.py" "$DIR/../../script/db_ingest.py" "$DIR/../../script"/dsl_*.py "$DIR/../../script/probe.py" "$DIR"
         . "$DIR/../../script/build.sh"
         ;;
