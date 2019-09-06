dfd38b174113b86c02b525f9637994f7bbe649fe
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-09-06 18:34:16 -0400

    update scripts

--- script/backup-image.sh
+++ script/backup-image.sh
@@ -4,9 +4,10 @@ DIR=$(dirname $(readlink -f "$0"))
 YML="$DIR/../deployment/docker-swarm/docker-compose.yml"
 
 mkdir -p "$DIR/../archive"
-for image in `grep 'image:' "$YML" | awk '{print$2}'` smtc_certificate:latest; do
+for image in `awk '/image:/{print $2}/docker run/{im=$NF;gsub(/\"/,"",im);print im}' "$YML"` smtc_certificate:latest; do
     imagefile=${image//\//-}
     imagefile=${imagefile//:/-}
     echo "archiving $image => $imagefile"
     (sudo docker image save "$image" > "$DIR/../archive/${imagefile}.tar") || (sudo docker pull "$image" && (sudo docker image save "$image" > "$DIR/../archive/${imagefile}.tar"))
 done
+
