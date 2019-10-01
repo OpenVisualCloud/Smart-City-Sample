4e9be1b507f6af63e1f19462a545430968cea858
vcse <maureen.m.yi@intel.com>
2019-10-01 09:01:58 -0700

    retrieve video from local storage

--- deployment/docker-swarm/smart-upload.m4
+++ deployment/docker-swarm/smart-upload.m4
@@ -3,10 +3,11 @@ define(`SERVICE_INTERVAL_SMART_UPLOAD',`60')dnl
     defn(`OFFICE_NAME')_smart_upload:
         image: smtc_smart_upload:latest
         environment:
-            QUERY: "time>=now-eval(defn(`SERVICE_INTERVAL_SMART_UPLOAD')*1000) where objects.detection.bounding_box.x_max-objects.detection.bounding_box.x_min>0.01"
+            QUERY: "time>=now-eval(defn(`SERVICE_INTERVAL_SMART_UPLOAD')*1000000) where objects.detection.bounding_box.x_max-objects.detection.bounding_box.x_min>0.01"
             INDEXES: "recordings,analytics"
             OFFICE: "defn(`OFFICE_LOCATION')"
             DBHOST: "http://ifelse(eval(defn(`NOFFICES')>1),1,defn(`OFFICE_NAME')_db,db):9200"
+            SMHOST: "http://defn(`OFFICE_NAME')_storage:8080/recording/"
             SERVICE_INTERVAL: defn(`SERVICE_INTERVAL_SMART_UPLOAD')
             UPDATE_INTERVAL: "5"
             SEARCH_BATCH: "3000"
--- maintenance/smart-upload/smart-upload.py
+++ maintenance/smart-upload/smart-upload.py
@@ -3,6 +3,7 @@
 from db_ingest import DBIngest
 from db_query import DBQuery
 from signal import signal, SIGTERM
+import requests
 import os
 import time
 
@@ -14,6 +15,20 @@ search_batch=int(os.environ["SEARCH_BATCH"])
 update_batch=int(os.environ["UPDATE_BATCH"])
 office=list(map(float, os.environ["OFFICE"].split(",")))
 dbhost=os.environ["DBHOST"]
+smhost=os.environ["SMHOST"]
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    print("local_filename: ", local_filename)
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    # f.flush()
+    return local_filename
 
 def quit_service(signum, sigframe):
     if dbs and rs: dbs.delete(rs["_id"])
@@ -43,12 +58,30 @@ while True:
     print("query = ", query)
 
     try:
-        updates=[]
         for q in dbq.search(query):
-            updates.append({"recording":q["_source"]["path"]})
-            print(q["_source"]["path"])
-        if updates:
-            print("update "+str(len(updates)))
+            filename=smhost+q["_source"]["path"]
+            #print("update: ", filename)
+            #print("time: ", (os.path.basename(filename).split('.')[0]) )
+            #print("office: ", str(office[0])+","+str(office[1]))
+
+            r=requests.get(filename, timeout=1)
+            if r.status_code!=200: 
+                print("pipeline status: "+str(r.status_code), flush=True)
+                print(r.text, flush=True)
+                break
+
+            print("get url: ", r.url)
+            local_filename = download_file(r.url)
+
+            # #add updates to request
+            # with open(filename,"rb") as fd:
+                # r=requests.post(sthost,data={
+                    # "time":str((os.path.basename(filename).split('.')[0])),
+                    # "office":str(office[0])+","+str(office[1]),
+            # },files={
+                    # "file": fd,
+                # },verify=False)
+            # #os.remove(filename)
 
     except Exception as e:
         print("Exception: "+str(e), flush=True)
