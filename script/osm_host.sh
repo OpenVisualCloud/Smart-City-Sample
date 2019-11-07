552fb4581d6c3798eb10613ba534942900c91277
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-11-07 08:57:17 -0800

    add --rm/flat-node options

--- script/osm_host.sh
+++ script/osm_host.sh
@@ -7,7 +7,9 @@ if test -z "$pbf"; then
 fi
 
 docker volume rm openstreetmap-data
+docker volume rm openstreetmap-nodes
 docker volume create openstreetmap-data
+docker volume create openstreetmap-nodes
 IMAGE="overv/openstreetmap-tile-server:1.1"
-docker run -v $(readlink -f "$pbf"):/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main ${IMAGE} import
-docker run -p 8080:80 -v openstreetmap-data:/var/lib/postgresql/10/main -d ${IMAGE} run
+docker run --rm $(env | grep -E '_(proxy)=' | sed 's/^/-e /') -v $(readlink -f "$pbf"):/data.osm.pbf -v openstreetmap-nodes:/nodes -v openstreetmap-data:/var/lib/postgresql/10/main -e "OSM2PGSQL_EXTRA_ARGS=--flat-nodes /nodes/flat_nodes.bin" ${IMAGE} import
+docker run --rm -p 8080:80 -v openstreetmap-data:/var/lib/postgresql/10/main --shm-size="192m" -d ${IMAGE} run
