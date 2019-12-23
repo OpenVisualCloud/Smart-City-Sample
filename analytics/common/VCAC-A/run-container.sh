9f2f59cf06727e7adf12eb06b2eba11525e38248
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-12-23 12:58:49 -0800

    fix run-container.sh

--- analytics/common/VCAC-A/run-container.sh
+++ analytics/common/VCAC-A/run-container.sh
@@ -7,12 +7,12 @@ function gracefully_exit {
 
 trap gracefully_exit SIGTERM
 
-# network setting
-NETWORK=$(docker inspect $HOSTNAME --format {{.NetworkSettings.Networks}} | sed -e 's/^map\[\(.*\)]/\1/' -e 's/\(.*\):.*/--network \1/')
+# setting
+ENVS="$(env | grep ^VCAC_ | sed 's/^VCAC_/-e /')"
+NETWORKS="$(docker inspect $HOSTNAME --format {{.NetworkSettings.Networks}} | sed -e 's/^map\[\(.*\)]/\1/' -e 's/\(.*\):.*/--network \1/')"
 BINDS="$(docker inspect $HOSTNAME --format {{.HostConfig.Mounts}} | sed -e 's/ \+/:/g' -e 's/:<nil>//g' -e 's/}:{/ /g' -e 's/\[{//' -e 's/}]//' -e 's/bind:/-v /g' -e 's/:true/:ro/g' -e 's/:false/:rw/g')"
 
 # docker run
-echo /usr/local/bin/docker-entrypoint.sh docker run $(env | grep ^VCAC_ | sed -e 's/^VCAC_\(.*\)=.*/-e \1/') --rm --name $HOSTNAME --user root -v /tmp:/tmp -v /var/tmp:/var/tmp --device=/dev/ion:/dev/ion --privileged $NETWORK $BINDS "$@" $VCAC_IMAGE
-/usr/local/bin/docker-entrypoint.sh docker run $(env | grep ^VCAC_ | sed -e 's/^VCAC_\(.*\)=.*/-e \1/') --rm --name $HOSTNAME --user root -v /tmp:/tmp -v /var/tmp:/var/tmp --device=/dev/ion:/dev/ion --privileged $NETWORK $BINDS "$@" $VCAC_IMAGE &
+/usr/local/bin/docker-entrypoint.sh docker run --rm --name $HOSTNAME --user root -v /tmp:/tmp -v /var/tmp:/var/tmp --device=/dev/ion:/dev/ion --privileged $ENVS $NETWORKS $BINDS "$@" $VCAC_IMAGE &
 
 wait
