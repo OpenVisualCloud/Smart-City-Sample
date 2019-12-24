d68790c3d82d31634effdd38696e4723510c36ad
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-12-23 16:10:44 -0800

    add volume mount/multiple networks

--- analytics/common/VCAC-A/run-container.sh
+++ analytics/common/VCAC-A/run-container.sh
@@ -5,12 +5,13 @@ function gracefully_exit {
     exit 0
 }
 
+# gracefully exit
 trap gracefully_exit SIGTERM
 
 # setting
 ENVS="$(env | grep ^VCAC_ | sed 's/^VCAC_/-e /')"
-NETWORKS="$(docker inspect $HOSTNAME --format {{.NetworkSettings.Networks}} | sed -e 's/^map\[\(.*\)]/\1/' -e 's/\(.*\):.*/--network \1/')"
-BINDS="$(docker inspect $HOSTNAME --format {{.HostConfig.Mounts}} | sed -e 's/ \+/:/g' -e 's/:<nil>//g' -e 's/}:{/ /g' -e 's/\[{//' -e 's/}]//' -e 's/bind:/-v /g' -e 's/:true/:ro/g' -e 's/:false/:rw/g')"
+NETWORKS="$(docker inspect $HOSTNAME --format {{.NetworkSettings.Networks}} | sed -e 's/map\[\(.*\)]/\1/' -e 's/\([A-Za-z0-9_]*\):[^ ]*/--network=\1/g')"
+BINDS="$(docker inspect $HOSTNAME --format {{.HostConfig.Mounts}} | sed -e 's/\[\(.*\)]/\1/' -e 's/{\([a-z]*\) \([^ ]*\) \([^ ]*\) \([a-z]*\)[^}]*}/-v \2:\3:\4/g' -e 's/:true/:ro/g' -e 's/:false/:rw/g')"
 
 # docker run
 /usr/local/bin/docker-entrypoint.sh docker run --rm --name $HOSTNAME --user root -v /tmp:/tmp -v /var/tmp:/var/tmp --device=/dev/ion:/dev/ion --privileged $ENVS $NETWORKS $BINDS "$@" $VCAC_IMAGE &
