95cd0e89c0fa164397c2319732b56b9f836d5aae
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-01-03 13:24:11 -0800

    update script fix for vcac-zone and ssh timeout

--- script/update-image.sh
+++ script/update-image.sh
@@ -1,27 +1,28 @@
 #!/bin/bash -e
 
-NODEPREFIX="172.32"
 function transfer_image {
     image="$1"
     nodeip="$2"
 
-    # trasnfer VCAC-A images to VCAC-A nodes only
-    if [[ -n "$(echo $nodeip | grep ${NODEPREFIX})" ]] || [[ "$(id -u)" -eq "0" ]]; then
-        worker="root@$nodeip"
-    else
-        worker="$nodeip"
-    fi
+    # overwrite vcac username
+    case "$3" in
+    *vcac-zone:yes*|*vcac_zone==yes*)
+        worker="root@$nodeip";;
+    *)
+        worker="$nodeip";;
+    esac
 
     echo "Update image: $image to $worker"
     sig1=$((docker image inspect -f {{.ID}} $image || ((docker pull $image 1>&2) && docker image inspect -f {{.ID}} $image)) | grep .)
     echo " local: $sig1"
 
-    sig2=$(ssh $worker "docker image inspect -f {{.ID}} $image 2> /dev/null || echo")
+    CONNECTION_TIMEOUT=1
+    sig2=$(ssh -o ConnectTimeout=$CONNECTION_TIMEOUT $worker "docker image inspect -f {{.ID}} $image 2> /dev/null || echo" || true)
     echo "remote: $sig2"
 
     if test "$sig1" != "$sig2"; then
         echo "Transfering image..."
-        docker save $image | ssh $worker "docker image rm -f $image 2>/dev/null; docker load"
+        (docker save $image | ssh -o ConnectTimeout=$CONNECTION_TIMEOUT $worker "docker image rm -f $image 2>/dev/null; docker load") || true
     fi
     echo ""
 }
@@ -40,7 +41,7 @@ for id in $(docker node ls -q 2> /dev/null); do
             # skip unavailable or manager node
             if test -z "$(hostname -I | grep --fixed-strings $nodeip)"; then
                 for image in $(awk -v labels="$labels node.role=${role}" -f "$DIR/scan-yaml.awk" "$YML"); do
-                    transfer_image $image "$nodeip"
+                    transfer_image $image "$nodeip" "$labels"
                 done
             fi
         fi
@@ -53,7 +54,7 @@ if [ -x /usr/bin/kubectl ] || [ -x /usr/local/bin/kubectl ]; then
         labels="$(kubectl describe node $id | awk '/Annotations:/{lf=0}/Labels:/{sub("Labels:","",$0);lf=1}lf==1{sub("=",":",$1);print$1}')"
 
         for image in $(awk -v labels="$labels" -f "$DIR/scan-yaml.awk" "${DIR}/../deployment/kubernetes"/*.yaml); do
-            transfer_image $image "$nodeip"
+            transfer_image $image "$nodeip" "$labels"
         done
     done
 fi
