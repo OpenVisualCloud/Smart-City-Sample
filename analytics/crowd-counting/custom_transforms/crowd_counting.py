54c72a4c2d3d3eb9502d5ff47631a78e160013b5
yimm0815 <maureen.m.yi@intel.com>
2020-01-03 12:21:38 -0800

    read model resolution from tensor

--- analytics/crowd-counting/custom_transforms/crowd_counting.py
+++ analytics/crowd-counting/custom_transforms/crowd_counting.py
@@ -6,35 +6,40 @@ from PIL import Image, ImageDraw
 
 class CrowdCounting:
     def __init__(self,zone=0,width=0,height=0, polygon=[]):
-        print("===========CrowdCounting:__init__:zone,width,height,polygon=", zone, width, height, polygon, "================")
-
         self.zone = zone
         self.mask=[0]
         self.crowd_count=0
         self.polygon=polygon
 
-        # model resolution 128x96
-        # adjust polygon size to model resolution
-        self.width = 128
-        self.height = 96
-        self.ratioX = self.width * 1.0 / width
-        self.ratioY = self.height * 1.0 / height
-        for t in range(len(self.polygon)>>1):
-            self.polygon[t<<1] = int(self.polygon[t<<1] * self.ratioX)
-            self.polygon[(t<<1)+1] = int(self.polygon[(t<<1)+1] * self.ratioY)
-
-        #convert polygon to mask algorithm
-        #https://stackoverflow.com/questions/3654289/scipy-create-2d-polygon-mask
-        self.img = Image.new('L', (self.width, self.height), 0)
-        ImageDraw.Draw(self.img).polygon(self.polygon, outline=1, fill=1)
-        self.mask = numpy.array(self.img).flatten()
+        self.sensor_width = width
+        self.sensor_height = height
+        self.model_width = 0
+        self.model_height = 0
 
     def process_frame(self, frame):
         for tensor in frame.tensors():
-            data = tensor.data()
-            imgData = []
-            imgData.append(tensor.data())
-            self.crowd_count = numpy.sum(self.mask * imgData)
+            if (self.model_width == 0):
+                #generate bitmask
+                self.model_height = tensor.dims()[1]
+                self.model_width = tensor.dims()[2]
+                self.ratioX = self.model_width * 1.0 / self.sensor_width
+                self.ratioY = self.model_height * 1.0 / self.sensor_height
+                
+                for t in range(len(self.polygon)>>1):
+                    self.polygon[t<<1] = int(self.polygon[t<<1] * self.ratioX)
+                    self.polygon[(t<<1)+1] = int(self.polygon[(t<<1)+1] * self.ratioY)
+
+                #convert polygon to mask algorithm
+                #https://stackoverflow.com/questions/3654289/scipy-create-2d-polygon-mask
+                self.img = Image.new('L', (self.model_width, self.model_height), 0)
+                ImageDraw.Draw(self.img).polygon(self.polygon, outline=1, fill=1)
+                self.mask = numpy.array(self.img).flatten()
+
+            else:
+                #zone based crowd counting
+                imgData = []
+                imgData.append(tensor.data())
+                self.crowd_count = numpy.sum(self.mask * imgData)
 
         if (self.crowd_count):
             messages = list(frame.messages())
