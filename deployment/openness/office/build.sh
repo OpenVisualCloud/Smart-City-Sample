d0bb9eb399f07c3d9c33bd1d6527f6d24f290075
ttao1 <tao.q.tao@intel.com>
2020-03-09 11:43:25 -0700

    Add multiple office support for OpenNESS

--- deployment/openness/office/build.sh
+++ deployment/openness/office/build.sh
@@ -9,6 +9,12 @@ IFS="," read -r -a NANALYTICS <<< "${5:-3}"
 FRAMEWORK="${6:-gst}"
 HOSTIP=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' --selector='node-role.kubernetes.io/master' | cut -f1 -d' ')
 
+IFS=',' read -a camera_host_array <<< $CAMERA_HOSTS
+if [ ${#camera_host_array[@]} -ne ${NOFFICES} ];then
+  echo "The number of camera hosts shall be the same number of offices!"
+  exit 0
+fi
+
 echo "Generating templates with PLATFORM=${PLATFORM}, SCENARIO=${SCENARIO}, NOFFICES=${NOFFICES}"
 find "${DIR}" -maxdepth 1 -name "*.yaml" -exec rm -rf "{}" \;
 for template in $(find "${DIR}" -maxdepth 1 -name "*.yaml.m4" -print); do
@@ -18,7 +24,7 @@ for template in $(find "${DIR}" -maxdepth 1 -name "*.yaml.m4" -print); do
             for ((OFFICEIDX=1;OFFICEIDX<=${NOFFICES};OFFICEIDX++)); do
                 if [[ -n $(echo 'include(office.m4)OFFICE_LOCATION' | m4 -I "$DIR" -DSCENARIO=${SCENARIO} -DSCENARIOIDX=${SCENARIOIDX} -DOFFICEIDX=${OFFICEIDX}) ]]; then
                     yaml=${template/.yaml.m4/-s${SCENARIOIDX}o${OFFICEIDX}.yaml}
-                    m4 -DNOFFICES=${NOFFICES} -DSCENARIO=${SCENARIO} -DPLATFORM=${PLATFORM} -DNCAMERAS=${NCAMERAS[0]} -DNCAMERAS2=${NCAMERAS[1]:-${NCAMERAS[0]}} -DNANALYTICS=${NANALYTICS[0]} -DNANALYTICS2=${NANALYTICS[1]:-${NANALYTICS[0]}} -DFRAMEWORK=${FRAMEWORK} -DOFFICEIDX=${OFFICEIDX} -DSCENARIOIDX=${SCENARIOIDX} -DUSERID=$(id -u) -DGROUPID=$(id -g) -DCAMERA_HOST=${CAMERA_HOST} -DCLOUD_HOST=${CLOUD_HOST} -I "${DIR}" "${template}" > "${yaml}"
+                    m4 -DNOFFICES=${NOFFICES} -DSCENARIO=${SCENARIO} -DPLATFORM=${PLATFORM} -DNCAMERAS=${NCAMERAS[0]} -DNCAMERAS2=${NCAMERAS[1]:-${NCAMERAS[0]}} -DNANALYTICS=${NANALYTICS[0]} -DNANALYTICS2=${NANALYTICS[1]:-${NANALYTICS[0]}} -DFRAMEWORK=${FRAMEWORK} -DOFFICEIDX=${OFFICEIDX} -DSCENARIOIDX=${SCENARIOIDX} -DUSERID=$(id -u) -DGROUPID=$(id -g) -DCAMERA_HOST=${camera_host_array[${OFFICEIDX}-1]} -DCLOUD_HOST=${CLOUD_HOST} -I "${DIR}" "${template}" > "${yaml}"
                 fi
             done
         done
--- deployment/openness/office/start.sh
+++ deployment/openness/office/start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -e
 
 DIR=$(dirname $(readlink -f "$0"))
+SCENARIO="${2:-traffic}"
 NOFFICES="${4:-1}"
 
 shift
 . "$DIR/build.sh"
-
-function create_secret {
-    kubectl create secret generic self-signed-certificate "--from-file=${DIR}/../../certificate/self.crt" "--from-file=${DIR}/../../certificate/self.key"
-}
 
 function create_and_deploy_office_db_key_pair {
     # create key pair for ssh to cloud db host machine
@@ -32,15 +29,11 @@ if [[ -z $CLOUD_HOST ]]; then
   exit 0
 fi
 
-if [[ -z $CAMERA_HOST ]]; then
+if [[ -z $CAMERA_HOSTS ]]; then
   echo "Please define environment variable CAMERA_HOST"
   exit 0
 fi
 
-
-# create secrets
-"$DIR/../../certificate/self-sign.sh"
-create_secret 2>/dev/null || (kubectl delete secret self-signed-certificate; create_secret)
 
 # create configmap
 kubectl create configmap sensor-info "--from-file=${DIR}/../../../maintenance/db-init/sensor-info.json"
