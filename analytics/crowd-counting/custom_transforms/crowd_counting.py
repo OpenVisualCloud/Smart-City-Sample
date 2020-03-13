7340876517109bccef9054fc5bbb3d9f8358a4b0
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-03-13 14:42:48 -0700

    skip frames in custom_transform

--- analytics/crowd-counting/custom_transforms/crowd_counting.py
+++ analytics/crowd-counting/custom_transforms/crowd_counting.py
@@ -3,6 +3,9 @@ import numpy
 import time
 import json
 from PIL import Image, ImageDraw
+import os
+
+every_nth_frame = int(os.environ["EVERY_NTH_FRAME"])
 
 class CrowdCounting:
     def __init__(self,width=0,height=0,zonemap=[]):
@@ -25,42 +28,44 @@ class CrowdCounting:
         self.sensor_height = height
         self.model_width = 0
         self.model_height = 0
+        self.fidx=0
 
     def process_frame(self, frame):
-        for tensor in frame.tensors():
-            if (self.model_width == 0):
-                #generate bitmask
+        if self.fidx % every_nth_frame == 0:
+            for tensor in frame.tensors():
+                if (self.model_width == 0):
+                    #generate bitmask
                 
-                self.model_height = tensor.dims()[1]
-                self.model_width = tensor.dims()[2]
-                self.ratioX = self.model_width * 1.0 / self.sensor_width
-                self.ratioY = self.model_height * 1.0 / self.sensor_height
+                    self.model_height = tensor.dims()[1]
+                    self.model_width = tensor.dims()[2]
+                    self.ratioX = self.model_width * 1.0 / self.sensor_width
+                    self.ratioY = self.model_height * 1.0 / self.sensor_height
                 
-                for z in range(len(self.polygon)):
-                    for t in range(len(self.polygon[z])>>1):
-                        self.polygon[z][t<<1] = int(self.polygon[z][t<<1] * self.ratioX)
-                        self.polygon[z][(t<<1)+1] = int(self.polygon[z][(t<<1)+1] * self.ratioY)
+                    for z in range(len(self.polygon)):
+                        for t in range(len(self.polygon[z])>>1):
+                            self.polygon[z][t<<1] = int(self.polygon[z][t<<1] * self.ratioX)
+                            self.polygon[z][(t<<1)+1] = int(self.polygon[z][(t<<1)+1] * self.ratioY)
 
-                    #convert polygon to mask algorithm
-                    #https://stackoverflow.com/questions/3654289/scipy-create-2d-polygon-mask
-                    self.img = Image.new('L', (self.model_width, self.model_height), 0)
-                    ImageDraw.Draw(self.img).polygon(self.polygon[z], outline=1, fill=1)
-                    self.mask[z] = numpy.array(self.img).flatten()
-            else:
-                #zone based crowd counting
-                imgData = []
-                imgData.append(tensor.data())
+                        #convert polygon to mask algorithm
+                        self.img = Image.new('L', (self.model_width, self.model_height), 0)
+                        ImageDraw.Draw(self.img).polygon(self.polygon[z], outline=1, fill=1)
+                        self.mask[z] = numpy.array(self.img).flatten()
+                else:
+                    #zone based crowd counting
+                    imgData = []
+                    imgData.append(tensor.data())
                 
-                for z in range(len(self.mask)):
-                    self.crowd_count[z] = numpy.sum(self.mask[z] * imgData)
-        if (self.crowd_count):
-            messages = list(frame.messages())
-            if len(messages) > 0:
-                json_msg = json.loads(messages[0].get_message())
-                count_dict = {}
-                for z in range(len(self.zone)):
-                    count_dict["zone"+str(self.zone[z])] = int(self.crowd_count[z])
-                json_msg["count"] = count_dict
-                messages[0].set_message(json.dumps(json_msg))
+                    for z in range(len(self.mask)):
+                        self.crowd_count[z] = numpy.sum(self.mask[z] * imgData)
+            if (self.crowd_count):
+                messages = list(frame.messages())
+                if len(messages) > 0:
+                    json_msg = json.loads(messages[0].get_message())
+                    count_dict = {}
+                    for z in range(len(self.zone)):
+                        count_dict["zone"+str(self.zone[z])] = int(self.crowd_count[z])
+                    json_msg["count"] = count_dict
+                    messages[0].set_message(json.dumps(json_msg))
                 
+        self.fidx=self.fidx+1 
         return True
