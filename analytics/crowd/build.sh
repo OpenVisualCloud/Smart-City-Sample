3dc100b596e7dbb204058c9394b32154ff4c4cd0
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-04-25 08:00:17 -0700

    warn ffmpeg in crowd and entrance

--- analytics/crowd/build.sh
+++ analytics/crowd/build.sh
@@ -2,7 +2,17 @@
 
 DIR=$(dirname $(readlink -f "$0"))
 SCENARIO="${2:-stadium}"
+FRAMEWORK="${6:-gst}"
 
+case "$FRAMEWORK" in
+    gst)
+        ;;
+    *)
+        echo "Not Implemented"
+        exit -1
+        ;;
+esac
+        
 case "$SCENARIO" in
     *stadium*)
         . "$DIR/../../script/build.sh"
