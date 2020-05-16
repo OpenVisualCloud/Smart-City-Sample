9b73f32bd0cdf51c3a6a54582f5356c9982452b5
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-05-16 09:42:06 -0700

    fix stop.sh

--- deployment/kubernetes/yaml/stop.sh
+++ deployment/kubernetes/yaml/stop.sh
@@ -3,7 +3,7 @@
 DIR=$(dirname $(readlink -f "$0"))
 
 for yaml in $(find "${DIR}" -maxdepth 1 -name "*.yaml" -print); do
-    kubectl delete -f "$yaml" --ignore-not-found=true 2>/dev/null || echo -n ""
+    kubectl delete --wait=false -f "$yaml" --ignore-not-found=true 2>/dev/null
 done
-kubectl delete secret self-signed-certificate 2> /dev/null || echo -n ""
+kubectl delete secret self-signed-certificate 2> /dev/null
 kubectl delete configmap sensor-info 2> /dev/null || echo -n ""
