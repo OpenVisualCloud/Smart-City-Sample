c7db1991e6ba4b5fe1239e3950e8dd790e162d46
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-05-29 21:55:34 -0700

    fix translation

--- maintenance/alert/language.py
+++ maintenance/alert/language.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 text={
-   "triggers": "触发条件",
-   "alert trigger": "警报服务",
+   "triggers": "触发服务",
+   "alert trigger": "警报触发",
    "active": "运行中",
    "server busy": "服务器忙碌: {}%",
    "server overload": "服务器过载: {}%",
-   "check sensor": "请检查录像头: 连接中断={}",
-   "imbalance": "负载失衡: {} 检测服务, {} 录像头",
-   "traffic busy": "交通拥挤: {} 物体",
-   "entrance crowded": "通道拥挤: {} 人",
-   "service slow": "队伍过长: {} 人",
-   "seat crowded": "人群拥挤: {} 人",
+   "check sensor": "请检查录像头: 连接中断{}",
+   "imbalance": "负载失衡: 检测服务{}, 摄像头{}",
+   "traffic busy": "交通拥挤: 物体{}",
+   "entrance crowded": "通道拥挤: 人{}",
+   "service slow": "队伍过长: 人{}",
+   "seat crowded": "人群拥挤: 人{}",
 }
