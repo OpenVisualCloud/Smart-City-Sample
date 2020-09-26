0b418ad6a981ed8c8e9ec9f553edd3edcf79e59b
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-09-26 09:58:13 -0700

    added auth placeholder

--- cloud/webc.py
+++ cloud/webc.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from tornado import ioloop, web
+from tornado.options import define, options, parse_command_line
+from auth import AuthHandler
 from signal import signal, SIGTERM, SIGQUIT
 from configuration import env
 from redirect import NGINXRedirect
@@ -15,12 +18,26 @@ def setup_scenarios():
     with open("/var/www/html/js/scenario.js","at") as fd:
         fd.write("scenarios.setting="+json.dumps(scenario.split(","))+";")
 
+tornado1=None
+
 def quit_service(signum, frame):
+    if tornado1: tornado1.add_callback(tornado1.stop)
     nginxc.stop()
         
 signal(SIGTERM, quit_service)
 setup_scenarios()
 
+app = web.Application([
+    (r'/api/auth',AuthHandler),
+])
+
+define("port", default=2222, help="the binding port", type=int)
+define("ip", default="127.0.0.1", help="the binding ip")
+parse_command_line()
+print("Listening to " + options.ip + ":" + str(options.port))
+app.listen(options.port, address=options.ip)
+
+tornado1=ioloop.IOLoop.instance();
 nginxc.start()
-nginxc.wait()
+tornado1.start()
 nginxc.stop()
