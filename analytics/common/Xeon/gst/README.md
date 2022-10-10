fed04266abdc1bedb5007787f8a5a38c866149f0
tedlu2021 <junjun.lu@intel.com>
2022-10-10 09:51:22 +0800

    download analytics/gst/Dockerfile when building

--- analytics/common/Xeon/gst/README.md
+++
@@ -1,5 +0,0 @@
-
-Dockerfile.1.gst
-
-is base on
-https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v22.3/Xeon/ubuntu-20.04/analytics/gst/Dockerfile
--- analytics/common/build.sh
+++ analytics/common/build.sh
@@ -2,4 +2,15 @@
 
 DIR=$(dirname $(readlink -f "$0"))
 
+download_image(){
+	URL=https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v22.3/Xeon/ubuntu-20.04/analytics/gst/Dockerfile
+	TARGET="${DIR}/Xeon/gst/Dockerfile.1.gst"
+	wget $URL -O $TARGET
+	sed -i '1i # smtc_analytics_base ' $TARGET
+	sed -i 's/v1.5.3/v1.4.1/g' $TARGET  #downgrade dlstreamer
+	sed -i '636d' $TARGET
+}
+
+
+download_image
 . "${DIR}/../../script/build.sh"
